a37eb683736e50f0aa5f3e032dc4e46938747edc
Vaibhav Pandole <vaibhav.pandole8244@gmail.com>
2025-03-21 01:16:00 +0530

    Update the requirements and setting.

--- medicare_connect/settings.py
+++ medicare_connect/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "medlink",
     "rest_framework",
+    "rest_framework_simplejwt",
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
