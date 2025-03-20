6cb5aa59cfdeb204f0c5373a81f2e5bdb4f290ea
Vaibhav Pandole <vaibhav.pandole8244@gmail.com>
2025-03-21 01:15:14 +0530

    Created models and their migrations.

--- medlink/models.py
+++ medlink/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Role(models.Model):
+    """
+    Custom model to store role of user.
+    """
+
+    ROLE_CHOICES = [
+        ("doctor", "Doctor"),
+        ("patient", "Patient"),
+    ]
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="role")
+    role = models.CharField(max_length=7, choices=ROLE_CHOICES, default="patient")
+
+
+class Patient(models.Model):
+    """
+    Model to store the patient information.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    medical_history = models.TextField(null=True)
+
+
+class Prescription(models.Model):
+    """
+    Model to store prescription assigned by doctor to patient.
+    """
+
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+    medication = models.CharField(max_length=255)
+    dosage = models.CharField(max_length=255)
+    instructions = models.TextField()
+    date_prescribed = models.DateTimeField(auto_now_add=True)
